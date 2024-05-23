560ece29d78c2ca7012734e68d9cb7e78b2abf4f
MISHOW2 <mshibambo63@gmail.com>
2024-05-23 16:45:35 +0200

    use product.js file

--- script/home.js
+++ script/home.js
@@ -1,26 +1,4 @@
-let product = [{
-  image: 'image/hoodie1front.jpeg',
-  name: 'SHAYALENTO HOODIE',
-  price: 400.00
-}];
 
-let tshirtProduct = [
-  {
-    image: 'image/t2-front.jpeg',
-    name: 'Black TEE',
-    priceCents: 200.00
-  },
-  {
-    image: 'image/T1-front.jpeg',
-    name: 'White TEE',
-    priceCents: 200.00
-  },
-  {
-    image: 'image/t6.jpg',
-    name: 'White TEE',
-    priceCents: 200.00
-  }
-];
 
 let productHTML = '';
 product.forEach((product) => {
